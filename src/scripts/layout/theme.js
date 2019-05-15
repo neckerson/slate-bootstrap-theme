a9111757144e505c12f53af111045d6d1f5902e8
Nate Eckerson <nate.eckerson@gmail.com>
2019-05-15 13:39:56 -0400

    Add Lazysizes & responsive-image snippets from shopify/starter-theme

--- src/scripts/layout/theme.js
+++ src/scripts/layout/theme.js
@@ -1,6 +1,13 @@
 import $ from 'jquery';
 import 'bootstrap';
 
+import 'lazysizes/plugins/object-fit/ls.object-fit';
+import 'lazysizes/plugins/parent-fit/ls.parent-fit';
+import 'lazysizes/plugins/rias/ls.rias';
+import 'lazysizes/plugins/bgset/ls.bgset';
+import 'lazysizes/plugins/respimg/ls.respimg';
+import 'lazysizes';
+
 import "../../styles/theme.scss";
 import "../../styles/theme.scss.liquid";
 
