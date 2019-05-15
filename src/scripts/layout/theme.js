9d1206b272e58104a515fa6febb5a3794fa77a6a
Nate Eckerson <nate.eckerson@gmail.com>
2019-05-15 13:16:28 -0400

    Import Popper.js, jQuery & Bootstrap.js

--- src/scripts/layout/theme.js
+++ src/scripts/layout/theme.js
@@ -1,2 +1,9 @@
+import $ from 'jquery';
+import 'bootstrap';
+
 import "../../styles/theme.scss";
 import "../../styles/theme.scss.liquid";
+
+// Set up jQuery so that it can be accessed from the HTML
+window.jQuery = $;
+window.$ = $;
