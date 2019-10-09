8ccd16542cf739701b474a7b79cb26487c99a40d
Nate Eckerson <nate.eckerson@gmail.com>
2019-10-09 10:45:03 -0400

    jquery-zoom shows full-size product image. Close #10
    The jquery-zoom plugin was not displaying the true full-size product
    image on mouseover. Instead it was attaching to an image displayed by
    the responsive-image snippet. This resulted in some zoom, but not the
    greatest amount possible.
    
    In this update to product.js, the full-size image URL is extracted from
    the active thumbnail element underneath the main product image, and
    passed to jquery-zoom.
    
    The JS regex assumes these thumbnails have _300x appended to the src
    element.

--- src/scripts/sections/product.js
+++ src/scripts/sections/product.js
@@ -215,8 +215,14 @@ register('product', {
   },
 });
 
-// This takes care of attaching jquery-zoom to the main image element, firing
-// when the image loads.
+// Below attaches jquery-zoom to the main image element, firing when the image
+// loads. The full-size image URL is extracted from the active product image,
+// and passed to jquery-zoom. It assumes the image has _300x appended to
+// the src element.
+
 $('.responsive-image__image').on('load', function() {
-  $('.product-image .responsive-image__wrapper:visible').zoom();
+  const reg = /_300x/;
+  let url = $('.product-image .responsive-image__wrapper:visible img')[0].src;
+  url = url.replace(reg, '');
+  $('.product-image .responsive-image__wrapper:visible').zoom({url});
 });
